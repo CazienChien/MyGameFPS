bd55a1b1d476f5da67ac96debbdfd36619e63c6b
Lê Hoàng Phúc <lehoangphuc0205@gmail.com>
2024-10-24 08:53:12 +0700

    part 1

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float mouseSensitivity = 500f;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {//lock con tro giua man hinh va lam no vo hinh
+        Cursor.lockState =  CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //input mouse
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //rotation around x axis ( Nhin len, xuong)
+        xRotation -= mouseY;
+
+        //clamp the rotation
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        //look left and right
+        yRotation += mouseX;
+
+        //Apply rotation to our transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+
+
+
+
+
     }
 }
+    
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    bool isMoving;
+
+
+    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //resetting the default velocity
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+
+        }
+        //lay input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //tao vector di chuyen
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //di chuyen that su
+        controller.Move(move * speed * Time.deltaTime);
+
+        //check if the player can jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //Actually jumping
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Falling down
+        velocity.y += gravity * Time.deltaTime;
+
+        //Executing the jump
+        controller.Move(velocity * Time.deltaTime);
+
+        if (lastPosition != gameObject.transform.position && isGrounded == true)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        lastPosition = gameObject.transform.position;    
+
     }
 }
