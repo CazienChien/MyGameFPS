979dc0c934aeaf5acf8a8798c35248717d6425c0
CazienChien <nson2570@gmail.com>
2024-11-16 23:06:58 +0700

    Add: Zombie Model, Animation, Animator

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int HP = 100;
+    private Animator animator;
+
+    private NavMeshAgent navAgent;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        HP -= damageAmount;
+
+        if (HP <= 0)
+        {
+            int randomValue = Random.Range(0, 2);
+
+            if(randomValue == 0)
+            {
+                animator.SetTrigger("Die1");
+            }
+            else
+            {
+                animator.SetTrigger("Die2");
+            }
+            GetComponent<CapsuleCollider>().enabled = false;
+
+        }
+        else
+        {
+            animator.SetTrigger("Damage");
+        }
+    }
+
+    private void Update()
+    {
+        if(navAgent.velocity.magnitude > 0.1f) 
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
